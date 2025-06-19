0445afcf4173c44ea7f64dc08425d1305b822475
marccejg <marcejgarcia@gmail.com>
2025-06-19 20:50:13 -0300

    OK

--- tragamonedas1.ts
+++ tragamonedas1.ts
@@ -1,11 +1,12 @@
 import { Jugador } from "./jugador";
 import { Tragamonedas } from "./tragamonedas";
+import { ConsoleColor } from "./consoleColor";
 
 export class TragamonedasLogo extends Tragamonedas {
   protected jugador: Jugador;
 
   constructor(jugador: Jugador) {
-    super("","Logo", ["♠", "♥", "♦", "♣"], 300, 500);
+    super("","Logo", ["♠",ConsoleColor.Red+ "♥"+ConsoleColor.Reset,ConsoleColor.Red+ "♦"+ConsoleColor.Reset, "♣"], 300, 500);
     this.jugador = jugador;
   }
 }
--- tragamonedas2.ts
+++ tragamonedas2.ts
@@ -1,11 +1,11 @@
 import { Jugador } from "./jugador";
 import { Tragamonedas } from "./tragamonedas";
-
+import { ConsoleColor } from "./consoleColor";
 export class TragamonedasNumeros extends Tragamonedas {
   protected jugador: Jugador;
 
   constructor(jugador: Jugador) {
-    super("","Logo", ["1", "2", "3", "4"],50, 150);
+    super("","Logo", [ConsoleColor.Red+"1"+ConsoleColor.Reset, ConsoleColor.Cyan+"2"+ConsoleColor.Reset, ConsoleColor.Green+"3"+ConsoleColor.Reset,ConsoleColor.Magenta+ "4"+ConsoleColor.Reset],50, 150);
     this.jugador = jugador;
   }
 }
