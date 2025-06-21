af12772e9d2628ccd0158a353db3544831229bcb
marccejg <marcejgarcia@gmail.com>
2025-06-20 21:12:48 -0300

    ok

--- consoleColor.ts
+++ consoleColor.ts
@@ -5,15 +5,6 @@ export enum ConsoleColor {
     Yellow = "\x1b[33m",
     Cyan = "\x1b[36m",
     Magenta = "\x1b[35m",
-    Reset = "\x1b[0m",
-    Bold = "\x1b[1m" ,
-    Dim = "\x1b[2m", 
-    Italic = "\x1b[3m", // cursiva
-    Underline = "\x1b[4m", //subrayado
-    Blink = "\x1b[5m ", //parpadea no funca
-    Inverse = "\x1b[7m", 
-    Hidden = "\x1b[8m",
-    Strikethrough = "\x1b[9m ",
-
+    Reset = "\x1b[0m"
 }
 //console.log(ConsoleColor.Red + "texto a colorear"+ ConsoleColor.Reset);
